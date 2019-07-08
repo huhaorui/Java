a506d6948b207807d38892faf2187facbcea34e5
huhaorui <43870434+huhaorui@users.noreply.github.com>
2019-07-08 23:40:29 +0800

    Initial commit

---
+++ EnjoyInSummerHoliday/t1/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n;
+        n=Integer.parseInt(in.next());
+        for (int x=0;x<n;x++)
+        {
+            int[] num=new int[5];
+            int sum=0;
+            for (int y=0;y<5;y++)
+            {
+                num[y]=Integer.parseInt(in.next());
+                sum+=num[y];
+            }
+            int t=26;
+            char c=(char)(sum%t+97);
+            System.out.println(c);
+        }
+        in.close();
+    }
+}
